a87c09cbcd8818ec87b41ea52df7c4949b3adf68
Joao Pedro <jpfdc9@gmail.com>
2021-06-23 18:15:56 -0300

    terceiro dia de nlw

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from 'express'
+import express, {Request, Response, NextFunction} from 'express'
 import 'reflect-metadata'
-import { router } from './services/routes'
-import "./database"
+import 'express-async-errors'
+
+import {router} from './services/routes'
+import './database'
 
 const app = express()
 
@@ -9,5 +11,19 @@ app.use(express.json())
 
 app.use(router)
 
-app.listen(3000, () => console.log('server is running'))
+app.use(
+	(err: Error, request: Request, response: Response, next: NextFunction) => {
+		if (err instanceof Error) {
+			return response.status(400).json({
+				error: err.message,
+			})
+		}
 
+		return response.status(500).json({
+			status: 'error',
+			message: 'Interna Server Error',
+		})
+	}
+)
+
+app.listen(3000, () => console.log('server is running'))
--- src/services/routes.ts
+++ src/services/routes.ts
@@ -1,12 +1,15 @@
-import { Router } from "express";
-import { CreateUserController } from "../controllers/CreateUserController";
-
+import {Router} from 'express'
+import {CreateUserController} from '../controllers/CreateUserController'
+import {CreateTagController} from '../controllers/CreateTagController'
+import {ensureAdmin} from '../middlewares/ensureAdmin'
 
 const router = Router()
 
 const cuc = new CreateUserController()
+const ctc = new CreateTagController()
 
+router.post('/users', cuc.handle)
 
-router.post("/users",  cuc.handle)
+router.post('/tags', ensureAdmin, ctc.handle)
 
 export {router}
